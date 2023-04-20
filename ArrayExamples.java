50cdfcaf25c052b1fd7d05bded3a49def26a7a14
BuckyGithub <77903497+BuckyGithub@users.noreply.github.com>
2023-04-20 09:39:22 -0700

    debugged ArrayExamples

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,14 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] dummy = new int[arr.length];
+
+    for(int i = 0; i < dummy.length; i++){
+      dummy[i] = arr[i];
+    }
+
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = dummy[arr.length - i - 1];
     }
   }
 
@@ -13,10 +19,11 @@ public class ArrayExamples {
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
+
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,14 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{5,4,3,2,1 }, input2);
+
+    int[] input3 = {1,2};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{2,1 }, input3);
 	}
 
 
@@ -14,5 +22,28 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1,2};
+    assertArrayEquals(new int[]{2,1}, ArrayExamples.reversed(input2));
+
+    int[] input3 = {1,2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input3));
   }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), .001);
+    double[] input2 = {1,2};
+    assertEquals(2.0, ArrayExamples.averageWithoutLowest(input2), .001);
+    double[] input3 = {1};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input3), .001);
+    double[] input4 = {1, 5, 5, 5};
+    assertEquals(5.0, ArrayExamples.averageWithoutLowest(input4), .001);
+    double[] input5 = {0,3,3,7,7,8,9};
+    assertEquals(6.1667, ArrayExamples.averageWithoutLowest(input5), .001);
+    double[] input6 = {0,0,0,3};
+    assertEquals(3, ArrayExamples.averageWithoutLowest(input6), .001);
+  }
+
 }
